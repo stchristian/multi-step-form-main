d35ab1a5e98007f27fdb318b86f641d7470b5d0b
Krisztian Hubner <krisztianhubner@gmail.com>
2024-02-02 19:02:26 +0100

    Removed div from label , fixing HTML validation

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -10,21 +10,22 @@ interface TextInputProps {
 
 export default function TextInput({ label, name, value, onChange, required, placeholder, error }: TextInputProps) {
   return (
-    <div className="mb-4">
-      <label className="flex flex-col">
-        <div className="text-blue-marine text-sm mb-1">{label}</div>
-        <input
-          type="text"
-          className={`border border-gray-light rounded-md px-4 py-2 ${error ? "border-red-strawberry" : ""}`}
-          name={name}
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-          aria-required={required}
-          placeholder={placeholder}
-          aria-describedby={error ? getMessageId(name) : undefined}
-          aria-invalid={!!error}
-        />
+    <div className="mb-4 flex flex-col">
+      <label className="text-blue-marine text-sm mb-1" htmlFor={name}>
+        {label}
       </label>
+      <input
+        type="text"
+        className={`border border-gray-light rounded-md px-4 py-2 ${error ? "border-red-strawberry" : ""}`}
+        name={name}
+        id={name}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        aria-required={required}
+        placeholder={placeholder}
+        aria-describedby={error ? getMessageId(name) : undefined}
+        aria-invalid={!!error}
+      />
       {error && (
         <span className="text-red-strawberry text-sm font-bold" id={getMessageId(name)}>
           {error}
